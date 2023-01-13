7af901ea8931dab31ae0c98bb595cfee7e139a09
25hlarsen4 <u1341754@umail.utah.edu>
2023-01-13 13:17:04 -0700

    Drafted the rest of the algorithm (not yet handling the possible error cases).

--- FormulaEvaluator/Evaluator.cs
+++ FormulaEvaluator/Evaluator.cs
@@ -17,7 +17,7 @@ namespace FormulaEvaluator
 
             foreach (String token in tokens)
             {
-                // if int
+                // if token is int
                 if (int.Parse(token) == 1)
                 {
                     if (operators.Peek() == "*" || operators.Peek() == "/")
@@ -41,7 +41,105 @@ namespace FormulaEvaluator
                 }
 
 
-                    
+                // if token is variable
+
+
+                // if token is + or -
+                if (token == "+" || token == "-")
+                {
+                    if (operators.Peek() == "+" || operators.Peek() == "-")
+                    {
+                        int val1 = vals.Pop();
+                        int val2 = vals.Pop();
+                        string op = operators.Pop();
+
+                        if (op == "+")
+                        {
+                            vals.Push(val1 + val2);
+                        }
+                        else
+                        {
+                            vals.Push(val1 - val2);
+                        }
+                    }
+
+                    operators.Push(token);
+                }
+
+
+                // if token is * or / or (
+                if (token == "*" || token == "/" || token == "(")
+                {
+                    operators.Push(token);
+                }
+
+
+                // if token is (
+                //if (token == "(")
+                //{
+                //    operators.Push(token);
+                //}
+
+
+                // if token is )
+                if (token == ")")
+                {
+                    if (operators.Peek() == "+" || operators.Peek() == "-")
+                    {
+                        int val1 = vals.Pop();
+                        int val2 = vals.Pop();
+                        string op = operators.Pop();
+
+                        if (op == "+")
+                        {
+                            vals.Push(val1 + val2);
+                        }
+                        else
+                        {
+                            vals.Push(val1 - val2);
+                        }
+                    }
+
+                    operators.Pop();
+
+                    if (operators.Peek() == "*" || operators.Peek() == "/")
+                    {
+                        int val1 = vals.Pop();
+                        int val2 = vals.Pop();
+                        string op = operators.Pop();
+
+                        if (op == "*")
+                        {
+                            vals.Push(val1 * val2);
+                        }
+                        else
+                        {
+                            vals.Push(val1 / val2);
+                        }
+                    }
+                }
+
+            }
+
+            if (operators.Count == 0)
+            {
+                return vals.Pop();
+            }
+
+            else
+            {
+                int val1 = vals.Pop();
+                int val2 = vals.Pop();
+                string op = operators.Pop();
+
+                if (op == "+")
+                {
+                    return val1 + val2;
+                }
+                else
+                {
+                    return val1 - val2;
+                }
             }
         }
 
