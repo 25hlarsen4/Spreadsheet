97589679d60cf238390200d18c698be5346be76f
25hlarsen4 <u1341754@umail.utah.edu>
2023-01-13 12:46:05 -0700

    Created the Evaluator class, created the testing class, and started the first condition of the evaluate algorithm.

---
+++ FormulaEvaluator/Evaluator.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Text.RegularExpressions;
+
+namespace FormulaEvaluator
+{
+    public static class Evaluator
+    {
+
+        public delegate int Lookup(String variable_name);
+
+        public static int Evaluate(String expression, Lookup variableEvaluator)
+        {
+            string[] tokens = Regex.Split(expression, "(\\()|(\\))|(-)|(\\+)|(\\*)|(/)");
+
+            Stack<int> vals = new Stack<int>();
+            Stack<string> operators = new Stack<string>();
+
+            foreach (String token in tokens)
+            {
+                // if int
+                if (int.Parse(token) == 1)
+                {
+                    if (operators.Peek() == "*" || operators.Peek() == "/")
+                    {
+                        int val = vals.Pop();
+                        string op = operators.Pop();
+                        if (op == "*")
+                        {
+                            vals.Push(val * int.Parse(token));
+                        }
+                        else
+                        {
+                            vals.Push(val / int.Parse(token));
+                        }
+                    }
+
+                    else
+                    {
+                        vals.Push(int.Parse(token));
+                    }
+                }
+
+
+                    
+            }
+        }
+
+
+    }
+}
