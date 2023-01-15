085773fc7eed6eb1bc39db593910497b975267fe
25hlarsen4 <u1341754@umail.utah.edu>
2023-01-14 18:34:57 -0700

    Began testing simple cases for all operators.

---
+++ FormulaEvaluatorTester/Program.cs
@@ -0,0 +1,70 @@
+﻿// + operator testing (no spaces)
+Console.WriteLine("3 + 5 = " + FormulaEvaluator.Evaluator.Evaluate("3+5", null));
+
+// + operator testing (with spaces)
+Console.WriteLine("3 + 5 = " + FormulaEvaluator.Evaluator.Evaluate("3 + 5", null));
+
+
+// - operator testing (no spaces)
+Console.WriteLine("5 - 3 = " + FormulaEvaluator.Evaluator.Evaluate("5-3", null));
+
+// - operator testing (with spaces)
+Console.WriteLine("5 - 3 = " + FormulaEvaluator.Evaluator.Evaluate("5 - 3", null));
+
+
+// * operator testing (no spaces)
+Console.WriteLine("3 * 5 = " + FormulaEvaluator.Evaluator.Evaluate("3*5", null));
+
+// * operator testing (with spaces)
+Console.WriteLine("3 * 5 = " + FormulaEvaluator.Evaluator.Evaluate("3 * 5", null));
+
+
+// / operator testing (no spaces)
+Console.WriteLine("10 / 5 = " + FormulaEvaluator.Evaluator.Evaluate("10/5", null));
+
+// / operator testing (with spaces)
+Console.WriteLine("10 / 5 = " + FormulaEvaluator.Evaluator.Evaluate("10 / 5", null));
+
+
+// parenthesis testing (no spaces)
+Console.WriteLine("(3 + 5) * 2 = " + FormulaEvaluator.Evaluator.Evaluate("(3+5)*2", null));
+
+// parenthesis testing (with spaces)
+Console.WriteLine("(3 + 5) * 2 = " + FormulaEvaluator.Evaluator.Evaluate("(3 + 5) * 2", null));
+
+
+// illegal expression testing below
+try
+{
+    FormulaEvaluator.Evaluator.Evaluate("3++5", null);
+} catch (ArgumentException)
+{
+    Console.WriteLine("invalid expression 3++5");
+}
+
+try
+{
+    FormulaEvaluator.Evaluator.Evaluate("5(3+5)", null);
+}
+catch (ArgumentException)
+{
+    Console.WriteLine("invalid expression 5(3+5)");
+}
+
+try
+{
+    FormulaEvaluator.Evaluator.Evaluate("5+3$", null);
+}
+catch (ArgumentException)
+{
+    Console.WriteLine("invalid expression 5+3$");
+}
+
+
+// 5++3     5(3+5)     5+3$
+
+
+
+Console.Read();
+
+
